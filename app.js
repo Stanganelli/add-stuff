df591e9e2f62c68de82f4cc3400ee1fbbe5ea60c
Enzo Victor Stanganelli Gama <64747700+stan899@users.noreply.github.com>
2022-08-08 18:00:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 let data = [
   {
     nome: "Enzo",
-    idade: 17
+    idade: 17,
+    hasAnos: true,
+    hastem: true,
     //mais chad
   },
   {
     nome: "Eric",
-    idade: 18
+    idade: 18,
+    hasAnos: true,
+    hastem: true,
   },
   {
     nome: "Allan",
-    idade: 17
+    idade: 42,
+    hasAnos: true,
+    hastem: true,
   },
   {
     nome: "Erica",
-    idade: 17
+    idade: 17,
+    hasAnos: true,
+    hastem: true,
   },
   {
     nome: "Aghata",
-    idade: 17
+    idade: 17,
+    hasAnos: true,
+    hastem: true,
   },
   {
     nome: "Seu nome",
-    idade: "Pode estar aqui"
+    idade: " Pode estar aqui",
+    hasAnos: false,
+    hastem: false,
   },
   /* ai só colocar o quanto mais quiser aqui  usando
     {
@@ -35,8 +47,20 @@ let data = [
 
 const info = document.querySelector("#info");
 let detalhes = data.map(function(item) {
-  return ("<div>" + item.nome + "" + " tem " + item.idade + " anos " + "</div>"
-    //alterando essa linha      
+  if (item.hasAnos) {
+    anos = " anos ";
+  }
+  else {
+    anos = "";
+  }
+  if (item.hastem) {
+    tem = " tem ";
+  }
+  else {
+    tem = "";
+  }
+  return ("<div>" + item.nome + "" + tem + item.idade + anos + "</div>"
+    //alterando essa linha    +++  
   )
 });
 info.innerHTML = detalhes.join("");
