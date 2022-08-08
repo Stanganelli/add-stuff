abb7f70c710e0f7f4dc10cb6048487c778fe2c4b
Enzo Victor Stanganelli Gama <64747700+stan899@users.noreply.github.com>
2022-08-08 17:43:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 let data = [
-    {
-        nome: "Enzo",
-        idade: 17
-//mais chad
-    },
-    {
-        nome: "Eric",
-        idade: 18
-    },
-    {
-        nome: "Allan",
-        idade: 17
-    },
-    {
-        nome: "Erica",
-        idade: 17
-    },
-    {
-        nome: "Aghata",
-        idade: 17
-    }, 
-/* ai só colocar o quanto mais quiser aqui  usando
   {
-        nome: "O que você quiser",
-        idade: 69
-    }, 
-
-que vai direto para o html 
-*/
+    nome: "Enzo",
+    idade: 17
+    //mais chad
+  },
+  {
+    nome: "Eric",
+    idade: 18
+  },
+  {
+    nome: "Allan",
+    idade: 17
+  },
+  {
+    nome: "Erica",
+    idade: 17
+  },
+  {
+    nome: "Aghata",
+    idade: 17
+  },
+  {
+    nome: "Seu nome",
+    idade: "Pode estar aqui"
+  },
+  /* ai só colocar o quanto mais quiser aqui  usando
+    {
+          nome: "O que você quiser",
+          idade: 69
+      }, 
+  que vai direto para o html 
+  */
 ];
 
-const info = document.querySelector("info");
-let detalhes = data.map(function(item){
-return (`<div>` = item.nome + "" + "é " + item.idade + " anos " + `</div>`
-//alterando essa linha      
-)
+const info = document.querySelector("#info");
+let detalhes = data.map(function(item) {
+  return ("<div>" + item.nome + "" + " tem " + item.idade + " anos " + "</div>"
+    //alterando essa linha      
+  )
 });
 info.innerHTML = detalhes.join("");
